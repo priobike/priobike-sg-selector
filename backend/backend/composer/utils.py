6b472b90bee9fbfb21739132c6793cedfa131d9c
adeveloper-wq <dajesch@gmail.com>
2023-04-25 16:46:01 +0200

    Add new measures to avoid duplicates

--- backend/backend/composer/utils.py
+++ backend/backend/composer/utils.py
@@ -62,7 +62,7 @@ def check_binding_exists(projected_lsa_linestring: LineString, projected_lsa_lin
         system_projected_binding_lsa_linestring = projected_binding_lsa_linestring.transform(
             settings.LONLAT, clone=True)
 
-        if system_projected_lsa_linestring.equals(system_projected_binding_lsa_linestring) and projected_lsa_linestring_id != binding.lsa.id:
+        if system_projected_lsa_linestring.equals(system_projected_binding_lsa_linestring) and projected_lsa_linestring_id == binding.lsa.id:
             print("\nDuplicate ID:")
             print(binding.lsa.id)
             return True
