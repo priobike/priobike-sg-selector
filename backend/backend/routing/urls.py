e086b380b7639ab27eaaa6bef0ba2002db41206d
adeveloper-wq <dajesch@gmail.com>
2024-02-20 11:11:42 +0100

    Fix URLs

--- backend/backend/routing/urls.py
+++ backend/backend/routing/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from routing.views import LSASelectionView, MultiLaneSelectionView, AllSGView
+from routing.views import LSASelectionView, MultiLaneSelectionView, AllSGViewGeo, AllSGViewMin
 
 app_name = "routing"
 
 urlpatterns = [
     path("select", LSASelectionView.as_view(), name="select"),
     path("select_multi_lane", MultiLaneSelectionView.as_view(), name="select_bulk"),
-    path("all", AllSGView.as_view(), name="all"),
+    path("all_geo", AllSGViewGeo.as_view(), name="all_geo"),
+    path("all_min", AllSGViewMin.as_view(), name="all_min"),
 ]
